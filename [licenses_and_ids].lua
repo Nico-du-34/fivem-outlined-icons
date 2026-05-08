bf148c2d5a74c7028bb1c9fa31f3d49cb8514753
Nico-du-34 <renardinio1237034@gmail.com>
2026-05-08 09:52:00 +0200

    Add [licenses and ids] icons and item definitions

---
+++ [licenses_and_ids].lua
@@ -0,0 +1,14 @@
+-- [licenses and ids]
+return {
+    ["certificate"] = { label = "Certificate" },
+    ["id_card"] = { label = "Id Card" },
+    ["lawyerpass"] = { label = "Lawyerpass" },
+    ["licence_weapon"] = { label = "Licence Weapon" },
+    ["license_driver"] = { label = "License Driver" },
+    ["license_fishing"] = { label = "License Fishing" },
+    ["license_hunting"] = { label = "License Hunting" },
+    ["license_marijuana_grow"] = { label = "License Marijuana Grow" },
+    ["license_marijuana_use"] = { label = "License Marijuana Use" },
+    ["lisence_pilot"] = { label = "Lisence Pilot" },
+    ["title"] = { label = "Title" },
+}
