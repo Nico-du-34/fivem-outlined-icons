f6ec04e6b22d93c52df84903054e3597c57a28bd
Nico-du-34 <renardinio1237034@gmail.com>
2026-05-08 09:49:24 +0200

    Add [camping] icons and item definitions

---
+++ [camping].lua
@@ -0,0 +1,12 @@
+-- [camping]
+return {
+    ["beachtowel"] = { label = "Beachtowel" },
+    ["campfire"] = { label = "Campfire" },
+    ["cooler"] = { label = "Cooler" },
+    ["firework1"] = { label = "Firework1" },
+    ["firework2"] = { label = "Firework2" },
+    ["firework3"] = { label = "Firework3" },
+    ["firework4"] = { label = "Firework4" },
+    ["firework5"] = { label = "Firework5" },
+    ["fogueira"] = { label = "Fogueira" },
+}
