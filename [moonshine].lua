1afaacb5db3486beebd2c912f0462f0538227dc1
Nico-du-34 <renardinio1237034@gmail.com>
2026-05-08 09:50:30 +0200

    Add [moonshine] icons and item definitions

---
+++ [moonshine].lua
@@ -0,0 +1,15 @@
+-- [moonshine]
+return {
+    ["empty_growler"] = { label = "Vide Growler" },
+    ["empty_growler2"] = { label = "Vide Growler2" },
+    ["emtpy_growler2"] = { label = "Emtpy Growler2" },
+    ["moonshine"] = { label = "Moonshine" },
+    ["moonshine_distill"] = { label = "Moonshine Distill" },
+    ["moonshine_ferm"] = { label = "Moonshine Ferm" },
+    ["moonshine_mash"] = { label = "Moonshine Mash" },
+    ["vodka_bootleg"] = { label = "Vodka Bootleg" },
+    ["vodka_distill"] = { label = "Vodka Distill" },
+    ["vodka_ferm"] = { label = "Vodka Ferm" },
+    ["vodka_mash"] = { label = "Vodka Mash" },
+    ["yeast"] = { label = "Yeast" },
+}
