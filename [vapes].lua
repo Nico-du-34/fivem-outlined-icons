b719f12ea4138edc1f76e88c01317af2bf35e3fa
Nico-du-34 <renardinio1237034@gmail.com>
2026-05-08 09:50:39 +0200

    Add [vapes] icons and item definitions

---
+++ [vapes].lua
@@ -0,0 +1,14 @@
+-- [vapes]
+return {
+    ["vape1"] = { label = "Vape1" },
+    ["vape2"] = { label = "Vape2" },
+    ["vape3"] = { label = "Vape3" },
+    ["vape4"] = { label = "Vape4" },
+    ["vape5"] = { label = "Vape5" },
+    ["vape_juice_apple"] = { label = "Vape Juice Apple" },
+    ["vape_juice_mango"] = { label = "Vape Juice Mango" },
+    ["vape_juice_menthol"] = { label = "Vape Juice Menthol" },
+    ["vape_juice_strawberry"] = { label = "Vape Juice Strawberry" },
+    ["vape_juice_tobacco"] = { label = "Vape Juice Tobacco" },
+    ["vape_juice_watermelon"] = { label = "Vape Juice Watermelon" },
+}
